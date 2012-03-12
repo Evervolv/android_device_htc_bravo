2ef137832d6ed9ba1815bde236b3d35e4181098d
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2012-03-12 00:06:27 -0500

    bravo: p10
    Change-Id: I43af7ddcddb03698b54c31675f7df290ccc342f3

--- ev.mk
+++ ev.mk
@@ -17,7 +17,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_bravo BUILD_FINGERPRINT=htc_wwe
 # Set up the product codename, build version & MOTD.
 #
 PRODUCT_CODENAME := Turba
-PRODUCT_VERSION_DEVICE_SPECIFIC := p9
+PRODUCT_VERSION_DEVICE_SPECIFIC := p10
 
 PRODUCT_MOTD :="\n\n\n--------------------MESSAGE---------------------\nThank you for choosing Evervolv for your HTC Desire\nPlease visit us at \#evervolv on irc.freenode.net\nFollow @preludedrew for the latest Evervolv updates\nGet the latest rom at evervolv.com\n------------------------------------------------\n"
 
