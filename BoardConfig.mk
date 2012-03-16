10802fef7b9a48ddfafe37d8cf0be192343c7584
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2012-03-16 05:05:32 -0500

    bravo: replace cflags with equivalent defines
    Change-Id: I06c8e1ab36d08a0b5155bf0697436e7c5266f84c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -37,12 +37,10 @@ TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_ARCH_VARIANT_CPU := cortex-a8
+TARGET_ARCH_VARIANT_FPU := neon
 ARCH_ARM_HAVE_TLS_REGISTER := true
 ARCH_ARM_HAVE_VFP := true
-
-# FPU compilation flags
-TARGET_GLOBAL_CFLAGS += -mtune=cortex-a8 -mfpu=neon -mfloat-abi=softfp
-TARGET_GLOBAL_CPPFLAGS += -mtune=cortex-a8 -mfpu=neon -mfloat-abi=softfp
 
 TARGET_SPECIFIC_HEADER_PATH := device/htc/bravo/include
 
